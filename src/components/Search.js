4818b4491c947165a58a226efcd2ff3875ca9776
Travis Houston <tjhouston14@gmail.com>
2018-08-09 22:24:17 +0200

    React Gallery

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router';
 
 import Container from './Container';
 import Navigation from './Navigation';
@@ -26,9 +27,10 @@ class Search extends Component {
 
 		this.setState({ query: this.passInput.input });
 
+		let inputVal = `/${this.option.value}`;
+		this.props.history.push(inputVal);
+
 		e.currentTarget.reset();
-
-
 
 	};
 
@@ -37,7 +39,7 @@ class Search extends Component {
 		return (
 			<div>
 				<form className="search-form" onSubmit={ this.handleSubmit }>
-					<input type="search" name="search" placeholder="Search" onChange={ this.onSearchChange } />
+					<input type="search" name="search" placeholder="Search" ref={(input) => this.option = input} onSubmit={ this.onSearchChange } />
 					<button type="submit" className="search-button">
                         <svg fill="#fff" height="24" viewBox="0 0 23 23" width="24" xmlns="http://www.w3.org/2000/svg">
                             <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
@@ -56,4 +58,4 @@ class Search extends Component {
     };
 }
 
-export default Search;
+export default withRouter(Search);
